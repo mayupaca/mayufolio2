e8eb2a7d15f4d2af97178c8e3d5ae24ad7184e30
Mayu Suzumura <47727645+mayupaca@users.noreply.github.com>
2024-02-26 11:30:01 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/* cSpell:ignore tailwindcss */
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
