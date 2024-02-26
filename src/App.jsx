b91bb4902e43910660dd98f5dd05889170f4d03a
Mayu Suzumura <47727645+mayupaca@users.noreply.github.com>
2024-02-26 12:25:06 -0500

    create header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
+/* cSpell:ignore mayu */
 import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <header className="text-gray-700 border-b border-gray-200">
+      <div className="container flex mx-auto p-5 flex-col md:flex-row items-center">
+        <a href="#" className="font-medium text-gray-900 mb-4 md:mb-0">
+          <span className="text-xl ml-3">MayuFolio</span>
+        </a>
+        <nav className="md:ml-auto text-base">
+          <a href="#home" className="mr-5 hover:text-blue-400 duration-300">
+            HOME
+          </a>
+          <a href="#about" className="mr-5 hover:text-blue-400 duration-300">
+            ABOUT
+          </a>
+          <a href="#skills" className="mr-5 hover:text-blue-400 duration-300">
+            SKILLS
+          </a>
+          <a href="#projects" className="hover:text-blue-400 duration-300">
+            PROJECTS
+          </a>
+        </nav>
+      </div>
+    </header>
+  );
 }
 
 export default App;
