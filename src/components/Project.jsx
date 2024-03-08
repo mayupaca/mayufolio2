4b1f1ba9adcad5637fd6a119126ca6010693d225
Mayu Suzumura <47727645+mayupaca@users.noreply.github.com>
2024-03-08 18:53:10 -0500

    created Icon.jsx file and moved svg icons

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { PiggyBankIcon, ArrowIcon } from "./Icon";
 
 function Project() {
   return (
@@ -12,16 +13,7 @@ function Project() {
             <div className="border-4 border-custom-gold rounded-lg p-8">
               <div className="flex items-center mb-3">
                 <div className="bg-[#AF9221] text-white rounded-full flex justify-center items-center w-8 h-8">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    fill="currentColor"
-                    class="bi bi-piggy-bank-fill"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M7.964 1.527c-2.977 0-5.571 1.704-6.32 4.125h-.55A1 1 0 0 0 .11 6.824l.254 1.46a1.5 1.5 0 0 0 1.478 1.243h.263c.3.513.688.978 1.145 1.382l-.729 2.477a.5.5 0 0 0 .48.641h2a.5.5 0 0 0 .471-.332l.482-1.351c.635.173 1.31.267 2.011.267.707 0 1.388-.095 2.028-.272l.543 1.372a.5.5 0 0 0 .465.316h2a.5.5 0 0 0 .478-.645l-.761-2.506C13.81 9.895 14.5 8.559 14.5 7.069q0-.218-.02-.431c.261-.11.508-.266.705-.444.315.306.815.306.815-.417 0 .223-.5.223-.461-.026a1 1 0 0 0 .09-.255.7.7 0 0 0-.202-.645.58.58 0 0 0-.707-.098.74.74 0 0 0-.375.562c-.024.243.082.48.32.654a2 2 0 0 1-.259.153c-.534-2.664-3.284-4.595-6.442-4.595m7.173 3.876a.6.6 0 0 1-.098.21l-.044-.025c-.146-.09-.157-.175-.152-.223a.24.24 0 0 1 .117-.173c.049-.027.08-.021.113.012a.2.2 0 0 1 .064.199m-8.999-.65a.5.5 0 1 1-.276-.96A7.6 7.6 0 0 1 7.964 3.5c.763 0 1.497.11 2.18.315a.5.5 0 1 1-.287.958A6.6 6.6 0 0 0 7.964 4.5c-.64 0-1.255.09-1.826.254ZM5 6.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0" />
-                  </svg>
+                  <PiggyBankIcon />
                 </div>
                 <h2 className="text-gray-900 text-lg font-medium ml-2">
                   Tiny Cash Book
@@ -33,20 +25,12 @@ function Project() {
                   financial management by tracking allowances and spending with
                   visual charts.
                 </p>
-                <a href="https://github.com/mayupaca/tiny-cash-book" className="flex mt-3 text-custom-gold items-center">
+                <a
+                  href="https://github.com/mayupaca/tiny-cash-book"
+                  className="flex mt-3 text-custom-gold items-center"
+                >
                   MORE
-                  <svg
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M15.0378 6.34317L13.6269 7.76069L16.8972 11.0157L3.29211 11.0293L3.29413 13.0293L16.8619 13.0157L13.6467 16.2459L15.0643 17.6568L20.7079 11.9868L15.0378 6.34317Z"
-                      fill="currentColor"
-                    />
-                  </svg>
+                  <ArrowIcon />
                 </a>
               </div>
             </div>
