c95bc4b61e812a5875a3d6ddb0a21edc84938ecd
Mayu Suzumura <47727645+mayupaca@users.noreply.github.com>
2024-03-13 14:31:56 -0400

    create data directory and projectsData.js file

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { PiggyBankIcon, ArrowIcon } from "./Icon";
+import { projects } from "../data/projectsData";
+import { ArrowIcon } from "./Icon";
 
 function Project() {
   return (
@@ -9,41 +10,41 @@ function Project() {
           Projects
         </h1>
         <div className="flex flex-wrap">
-          <div className="md:w-1/3 p-4">
-            <div className="border-4 border-custom-gold rounded-lg pt-5">
-              <div className="flex justify-center items-center w-full h-full">
-                <img
-                  src="./img/demoVideogif.gif"
-                  alt=""
-                  className="w-48 h-32 object-cover"
-                />
-              </div>
-              <div className="p-5">
-                <div className="flex items-center mb-3">
-                  <div className="bg-[#AF9221] text-white rounded-full flex justify-center items-center w-8 h-8">
-                    <PiggyBankIcon />
-                  </div>
-                  <h2 className="text-gray-900 text-lg font-medium ml-2">
-                    Tiny Cash Book
-                  </h2>
+          {projects.map((project, index) => (
+            <div key={index} className="md:w-1/3 p-4">
+              <div className="border-4 border-custom-gold rounded-lg pt-5">
+                <div className="flex justify-center items-center w-full h-full">
+                  <img
+                    src={project.image}
+                    alt={project.alt}
+                    className="w-48 h-32 object-cover"
+                  />
                 </div>
-                <div>
-                  <p>
-                    About Tiny Cash Book is a simple Python app that teaches
-                    kids financial management by tracking allowances and
-                    spending with visual charts.
-                  </p>
-                  <a
-                    href="https://github.com/mayupaca/tiny-cash-book"
-                    className="flex mt-3 text-custom-gold items-center"
-                  >
-                    MORE
-                    <ArrowIcon />
-                  </a>
+                <div className="p-5">
+                  <div className="flex items-center mb-3">
+                    <div className="bg-[#AF9221] text-white rounded-full flex justify-center items-center w-8 h-8">
+                      {project.icon}
+                    </div>
+                    <h2 className="text-gray-900 text-lg font-medium ml-2">
+                      {project.name}
+                    </h2>
+                  </div>
+                  <div>
+                    <p>
+                    {project.description}
+                    </p>
+                    <a
+                      href={project.git_repo}
+                      className="flex mt-3 text-custom-gold items-center"
+                    >
+                      MORE
+                      <ArrowIcon />
+                    </a>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
     </section>
