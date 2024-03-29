c090b7306e0084796dc1e1e720b3707851f52068
Mayu Suzumura <47727645+mayupaca@users.noreply.github.com>
2024-03-29 13:46:33 -0400

    change skill design

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,52 +1,29 @@
 import React from "react";
+import SkillLevel from "./SkillLevel";
 
 function Skill() {
   return (
     <section id="skill" className="text-gray-700 border-t border-gray-200">
       <div className="container px-5 py-24 mx-auto">
-        {/* <div className="mb-10 lg:mb-0 w-full lg:w-1/2 flex justify-center">
-          <img src="./img/takachiho.jpg" alt="" className="rounded" />
-        </div> */}
-
         <h1 className="text-2xl sm:text-3xl font-medium text-gray-900 mb-10 text-center lg:text-left">
           My Skill
         </h1>
         <div className="lg:pl-10 lg:py-6 w-full lg:w-1/2">
-          <h2>HTML</h2>
-          <div className="shadow bg-[#eadb9f]">
-            <div
-              className="bg-[#AF9221] text-xs leading-none py-1 text-center text-white"
-              style={{ width: "85%" }}
-            >
-              85%
-            </div>
+          <div className="flex items-center mb-2">
+            <h2 className="mr-2 w-24">HTML</h2>
+            <SkillLevel level={4} />
           </div>
-          <h2>CSS</h2>
-          <div className="shadow bg-[#eadb9f]">
-            <div
-              className="bg-[#AF9221] text-xs leading-none py-1 text-center text-white"
-              style={{ width: "85%" }}
-            >
-              85%
-            </div>
+          <div className="flex items-center mb-2">
+            <h2 className="mr-2 w-24">CSS</h2>
+            <SkillLevel level={4} />
           </div>
-          <h2>Python</h2>
-          <div className="shadow bg-[#eadb9f]">
-            <div
-              className="bg-[#AF9221] text-xs leading-none py-1 text-center text-white"
-              style={{ width: "70%" }}
-            >
-              70%
-            </div>
+          <div className="flex items-center mb-2">
+            <h2 className="mr-2 w-24">Python</h2>
+            <SkillLevel level={3} />
           </div>
-          <h2>React</h2>
-          <div className="shadow bg-[#eadb9f] ">
-            <div
-              className="bg-[#AF9221] text-xs leading-none py-1 text-center text-white"
-              style={{ width: "75%" }}
-            >
-              75%
-            </div>
+          <div className="flex items-center mb-2">
+            <h2 className="mr-2 w-24">React</h2>
+            <SkillLevel level={3} />
           </div>
         </div>
       </div>
---
+++ src/components/SkillLevel.jsx
@@ -0,0 +1,16 @@
+function SkillLevel({ level }) {
+  const totalCircles = 6;
+  const circles = [];
+
+  for (let i = 0; i < totalCircles; i++) {
+    circles.push(
+      <span key={i} className={i < level ? "text-[#AF9221] text-3xl ml-4" : "text-gray-400 text-3xl ml-4"}>
+        ●
+      </span>
+    );
+  }
+
+  return <div className="flex">{circles}</div>;
+}
+
+export default SkillLevel;
